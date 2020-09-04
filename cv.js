311d45196860510e73cb41cdc293c49fc4a79a1e
80010-80-07 <80010-80-07@exchange.ad.afpanet>
2020-09-04 12:21:41 +0200

    yop

--- cv.js
+++ cv.js
@@ -1,9 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
+ /* document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems);
-  });
+  });*/
+
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
+
+var grd = ctx.createRadialGradient(100, 100, 5, 100, 100, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, 600, 400); 
